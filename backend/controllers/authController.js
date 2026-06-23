c4d69960dc45ae7a9cdcc28b633fb97adafb4194
Fernando Bautista Paniagua <fb6583574@gmail.com>
2026-06-23 15:21:42 -0600

    Actulizacion poco de conexion de back y front

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,46 @@
+const db = require('../database/db');
+
 exports.login = (req, res) => {
 
-    const { correo, password } = req.body;
+    const { usuario, password } = req.body;
 
-    res.json({
-        mensaje: 'Login funcionando',
-        correo
-    });
+    const sql = `
+        SELECT *
+        FROM usuario
+        WHERE correo = ?
+        AND contraseña = ?
+    `;
+
+    db.query(
+        sql,
+        [usuario, password],
+        (err, result) => {
+
+            if (err) {
+                console.error(err);
+
+                return res.status(500).json({
+                    success: false,
+                    mensaje: 'Error del servidor'
+                });
+            }
+
+            if (result.length > 0) {
+
+                return res.json({
+                    success: true,
+                    mensaje: 'Login correcto',
+                    usuario: result[0]
+                });
+
+            }
+
+            res.json({
+                success: false,
+                mensaje: 'Correo o contraseña incorrectos'
+            });
+
+        }
+    );
 
 };
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 require('./database/db');
 
 const app = express();
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 app.use('/api/auth', require('./routes/auth.routes'));
-app.use('/api/posgrado', require('./routes/posgrado.routes'));
 
 app.get('/', (req, res) => {
-    res.send('Servidor funcionando');
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Servidor ejecutándose en puerto 3000');
+app.listen(PORT, () => {
+    console.log(`Servidor listo en http://localhost:${PORT}`);
 });
