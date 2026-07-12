21e140f993d4f00ac84459bae50f0b5d3979a794
Yzy-zu <knzz.uwu@gmail.com>
2026-07-12 13:37:11 -0600

    v1,0

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'fie',
+    password: 'fie',
     database: 'posgrado'
 });
 
